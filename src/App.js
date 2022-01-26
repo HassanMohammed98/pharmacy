054b472be421effa66c9e6d46d22301d2149cdae
Hassan Mohammed <hassan-n98@hotmail.com>
2022-01-26 17:47:20 +0300

    done first task home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./products.js";
+import products from "./products.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text">meds4u</h1>
+      <h4 className="text">get ur meds</h4>
+      <img
+        alt="Pharmacy Shop"
+        src="https://globalcarehospital.com/wp-content/uploads/2020/04/arredi-farmacie-min-olcdbjm9lkfarlthxu7qceilefhwmv8ravp8czhnr4-1024x512.jpg"
+        className="shop-image"
+      />
+      <div className="row">
+        <div>
+          <img
+            alt="Pharmacy Shop"
+            src={products[0].image}
+            className="prod-image"
+          />
+          <h3 className="text prod-text">{products[0].name}</h3>
+          <p className="text prod-text">{products[0].price}</p>
+        </div>
+        <div>
+          <img
+            alt="Pharmacy Shop"
+            src={products[1].image}
+            className="prod-image"
+          />
+          <h3 className="text prod-text">{products[1].name}</h3>
+          <p className="text prod-text">{products[1].price}</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "Panadol",
+    price: `${2.5} KD`,
+    image:
+      "https://www.dowa.co/content/images/thumbs/0007551_panadol-advance-500mg-48tabs_600.jpeg",
+  },
+  {
+    name: "Strepsils",
+    price: `${3} KD`,
+    image:
+      "https://www.luluhypermarket.com/medias/14744-01.jpg-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w0OTA4NjR8aW1hZ2UvanBlZ3xpbWFnZXMvaDgxL2hhYy85MDc4NTY5NjY0NTQyLmpwZ3w5MWVkOTcwYzI5MDE2Yjc1MjA2NWE0MmMwN2ZjZWYyOWVhMmZiZGNiNDkxMDRlY2Y5Y2MwY2UxMzlhNWE2MjNm",
+  },
+];
+
+export default products;
