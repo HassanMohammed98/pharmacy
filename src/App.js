ff801e8e1ae253a30bf47def312e0e58a1e6fd1f
Hassan Mohammed <hassan-n98@hotmail.com>
2022-01-27 16:54:39 +0300

    done second task Product List 27/01/2022

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
-import "./products.js";
+// import "./products.js";
 import products from "./products.js";
 
 function App() {
+  const productsList = products.map((product) => (
+    <div>
+      <img
+        alt={`Product's Image (${product.name})`}
+        src={product.image}
+        className="prod-image"
+      />
+      <h3 className="text prod-text">{product.name}</h3>
+      <p className="text prod-text">{product.price}</p>
+    </div>
+  ));
   return (
     <div>
       <h1 className="text">meds4u</h1>
@@ -12,26 +23,7 @@ function App() {
         src="https://globalcarehospital.com/wp-content/uploads/2020/04/arredi-farmacie-min-olcdbjm9lkfarlthxu7qceilefhwmv8ravp8czhnr4-1024x512.jpg"
         className="shop-image"
       />
-      <div className="row">
-        <div>
-          <img
-            alt="Pharmacy Shop"
-            src={products[0].image}
-            className="prod-image"
-          />
-          <h3 className="text prod-text">{products[0].name}</h3>
-          <p className="text prod-text">{products[0].price}</p>
-        </div>
-        <div>
-          <img
-            alt="Pharmacy Shop"
-            src={products[1].image}
-            className="prod-image"
-          />
-          <h3 className="text prod-text">{products[1].name}</h3>
-          <p className="text prod-text">{products[1].price}</p>
-        </div>
-      </div>
+      <div className="row">{productsList}</div>
     </div>
   );
 }
